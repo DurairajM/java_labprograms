8a8d34e5c45b816e2e06d43081b5e0d2d3dde9fd
Durairaj M <40629958+DurairajM@users.noreply.github.com>
2018-09-19 21:52:13 +0530

    Add files via upload

---
+++ ex142.java
@@ -0,0 +1,18 @@
+package lab_programs;
+import java.io.*;
+public class ex142 {
+  
+		 public static void main(String args[])throws Exception{  
+		  FileOutputStream fout1=new FileOutputStream("f1.txt");  
+		  FileOutputStream fout2=new FileOutputStream("f2.txt");  
+		  
+		  ByteArrayOutputStream bout=new ByteArrayOutputStream();  
+		  bout.write(145);  
+		  bout.writeTo(fout1);  
+		  bout.writeTo(fout2);  
+		  
+		  bout.flush();  
+		  bout.close();//has no effect  
+		  System.out.println("success...");  
+		 }  
+		}   
---
+++ ex148.java
@@ -0,0 +1,20 @@
+package lab_programs;
+
+import java.io.FileOutputStream;
+import java.io.*;
+
+public class ex148 {
+	public static void main(String args[]){  
+		   try{  
+	     FileReader fin=new FileReader("fw.txt");    
+		     //byte b[]=s.getBytes();//converting string into byte array  
+		       int i;
+		       while((i=fin.read())!=-1)
+		       {
+		    	   System.out.print((char)i);
+		       }
+		     fin.close();  
+		     System.out.println(" success...");  
+		    }catch(Exception e){System.out.println(e);}  
+		  }  
+}
