92b3c05b333408a496c42d3667b2a506c5bae0a4
Durairaj M <40629958+DurairajM@users.noreply.github.com>
2018-09-19 21:48:24 +0530

    Add files via upload

---
+++ ex106.java
@@ -0,0 +1,19 @@
+package lab_programs;
+
+public class ex106 {
+public static void main(String ar[])
+{
+	try
+	{
+		int a=10/0;	
+	}
+	catch(ArithmeticException e)
+	{
+		System.out.println(e);
+	}
+	finally
+	{
+		System.out.println("All the codes are executed");
+	}
+}
+}
